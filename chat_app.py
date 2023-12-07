fe74d73afe97bd9387fcb682b0a50b83a124e770
0aaryan <aroraryan826@gmail.com>
2023-12-07 15:28:02 +0530

    updating requrements

--- chat_app.py
+++ chat_app.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 import os
 from arduino_reader import ArduinoReader
 
-
 # AI imports
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import (
@@ -13,9 +12,10 @@ from langchain.schema import (
     AIMessage
 )
 
-
-arduino_reader = ArduinoReader(port='/dev/ttyUSB0', baud_rate=9600)
-
+try:
+    arduino_reader = ArduinoReader(port='/dev/ttyUSB0', baud_rate=9600)
+except:
+    arduino_reader = None
 def init():
     st.set_page_config(
         page_title="HealthMate Chat ⚕️",
@@ -43,7 +43,7 @@ def ai_chat():
     st.markdown("Welcome to HealthMate Chat, where you can get personalized health advice!")
     st.markdown('---')
 
-    chat = ChatOpenAI(temperature=0,model="gpt-3.5-turbo-1106")
+    chat = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-1106")
 
     # Initialize message history
     if "messages" not in st.session_state:
@@ -75,58 +75,64 @@ def ai_chat():
         st.write("Choose how to enter your health data.")
         health_data_method = st.selectbox("How do you want to enter your health data?", ("Enter Manually", "Read from Arduino"))
 
-        if health_data_method == "Enter Manually":
-            # Get all the health data; use keys from dict
-            for key in st.session_state["health_data"]:
-                # Take input for each key
-                st.session_state["health_data"][key] = st.number_input(label=key, value=float(st.session_state["health_data"][key]))
+        try:
+            if health_data_method == "Enter Manually":
+                # Get all the health data; use keys from dict
+                for key in st.session_state["health_data"]:
+                    # Take input for each key
+                    st.session_state["health_data"][key] = st.number_input(label=key, value=float(st.session_state["health_data"][key]))
 
-        elif health_data_method == "Read from Arduino":
-            st.subheader("Arduino Data Reading")
-            duration_sec = st.number_input("Duration of data reading (seconds):", value=10)
-            if st.button("Start Reading"):
-                with st.spinner("Reading data from Arduino..."):
-                    # Read data from the Arduino for 10 seconds
-                    avg_heart_rate, avg_temperature = arduino_reader.read_data_avg(duration_sec)
-                    # Update the health data
-                    st.session_state["health_data"]["Heart Rate"] = avg_heart_rate
-                    st.session_state["health_data"]["Temperature"] = avg_temperature
-                    st.success("Data read successfully!")
+            elif health_data_method == "Read from Arduino":
+                st.subheader("Arduino Data Reading")
+                duration_sec = st.number_input("Duration of data reading (seconds):", value=10)
+                if st.button("Start Reading"):
+                    with st.spinner("Reading data from Arduino..."):
+                        # Read data from the Arduino for 10 seconds
+                        if arduino_reader is None:
+                            st.error("Arduino not connected!")
+                            st.stop()
+                        avg_heart_rate, avg_temperature = arduino_reader.read_data_avg(duration_sec)
+                        # Update the health data
+                        st.session_state["health_data"]["Heart Rate"] = avg_heart_rate
+                        st.session_state["health_data"]["Temperature"] = avg_temperature
+                        st.success("Data read successfully!")
 
-                # Show the data
-                st.subheader("Data Read from Arduino")
-                st.write(f"Average Heart Rate: {avg_heart_rate}")
-                st.write(f"Average Temperature: {avg_temperature}")
-                
+                    # Show the data
+                    st.subheader("Data Read from Arduino")
+                    st.write(f"Average Heart Rate: {avg_heart_rate}")
+                    st.write(f"Average Temperature: {avg_temperature}")
 
-        # Add field
-        st.markdown('---')
-        st.title("➕ Add Field")
-        st.write("Add a new health data field.")
-        col1, col2 = st.columns(2)
-        with col1:
-            name = st.text_input("Name").capitalize()
-        with col2:
-            value = st.text_input("Value")
-        if st.button("Add"):
-            # Get keys from dict
-            names = st.session_state["health_data"].keys()
-            if name in names:
-                st.error("Field already exists")
-            else:
-                st.session_state["health_data"][name] = value
+            # Add field
+            st.markdown('---')
+            st.title("➕ Add Field")
+            st.write("Add a new health data field.")
+            col1, col2 = st.columns(2)
+            with col1:
+                name = st.text_input("Name").capitalize()
+            with col2:
+                value = st.text_input("Value")
+            if st.button("Add"):
+                # Get keys from dict
+                names = st.session_state["health_data"].keys()
+                if name in names:
+                    st.error("Field already exists")
+                else:
+                    st.session_state["health_data"][name] = value
+                    st.rerun()
+
+            # Remove field
+            st.markdown('---')
+            st.title("➖ Remove Field")
+            st.write("Remove an existing health data field.")
+            name = st.selectbox("Name", list(st.session_state["health_data"].keys()))
+
+            if st.button("Remove"):
+                del st.session_state["health_data"][name]
                 st.rerun()
 
-        # Remove field
-        st.markdown('---')
-        st.title("➖ Remove Field")
-        st.write("Remove an existing health data field.")
-        name = st.selectbox("Name", list(st.session_state["health_data"].keys()))
+        except Exception as e:
+            st.error(f"Error processing health data: {str(e)}")
 
-        if st.button("Remove"):
-            del st.session_state["health_data"][name]
-            st.rerun()
-        
     chat_placeholder = st.empty()
     with chat_placeholder.container():
         messages = st.session_state.get('messages', [])
@@ -137,7 +143,7 @@ def ai_chat():
             else:
                 message(msg.content, is_user=False, key=str(i) + '_ai')
 
-        #clear chat history
+        # clear chat history
         if st.button("Clear Chat"):
             st.session_state.messages = [
                 SystemMessage(content="You are the best doctor; you can answer any question asked to you. You also like to give advice and motivate people to live a healthy life and you also have a good sense of humor. you also like to use emojis in your conversation."),
